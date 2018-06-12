433e2ee1b24e44f5317c50b2b00466020f654959
Luiz Fernando França <luiz.darthvader@gmail.com>
2018-06-12 10:34:56 -0300

    asddd

--- css/app.js
+++ css/app.js
@@ -1,3 +1,43 @@
-function func() {
-    document.getElementById('p3').classList.add('slideOutRight');
-}
+
+$(document).ready(function(){
+
+    var animationEnd = (function(el) {
+        var animations = {
+            animation: 'animationend',
+            OAnimation: 'oAnimationEnd',
+            MozAnimation: 'mozAnimationEnd',
+            WebkitAnimation: 'webkitAnimationEnd',
+        };
+
+        for (var t in animations) {
+            if (el.style[t] !== undefined) {
+                return animations[t];
+            }
+        }
+    })(document.createElement('p3'));
+
+    $('#p3').one(animationEnd, doSomething);
+
+    function doSomething() {
+        $("#p1").animate({
+            opacity: 0.0,
+            left: "+=50",
+            easing: "easein"
+        }, 5000, function () {
+            //acabou a animacao faça alguma merda ou nao
+            console.log('its over');
+        });
+
+        $( "#p3" ).animate({
+            opacity: 0.0,
+            left: "+=50",
+            easing: "easein"
+        }, 500, function() {
+            //acabou a animacao faça alguma merda ou nao
+        });
+    }
+
+
+});
+
+
